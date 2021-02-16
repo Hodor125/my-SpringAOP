c81aa75fedbdfac14fb31755329e26898b9848aa
hodor <215338572@qq.com>
2021-02-16 16:13:12 +0800

    init

---
+++ src/test/java/com/hodor/test/TestSpringIoc.java
@@ -0,0 +1,22 @@
+package com.hodor.test;
+
+import com.hodor.controller.OrderController;
+import org.junit.Test;
+import org.springframework.container.ClassPathXmlApplicationContext;
+
+/**
+ * @author ：hodor007
+ * @date ：Created in 2021/2/7
+ * @description ：
+ * @version: 1.0
+ */
+public class TestSpringIoc {
+    //测试获取包扫描路径
+    @Test
+    public void test1() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        OrderController orderController = (OrderController) context.getBean(OrderController.class);
+//        orderController.findOrders();
+        orderController.addOrder();
+    }
+}
